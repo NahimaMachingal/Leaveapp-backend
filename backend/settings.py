eec611a51086100918d0bd46aa6ff403cade4771
NahimaMachingal <151160235+NahimaMachingal@users.noreply.github.com>
2025-01-25 21:39:15 +0400

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['leaveapp-backend-rkl7.onrender.com', '.onrender.com']
+
 
 
 
